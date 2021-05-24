9bdb09ac404128af0cd42aad8cd5b1791747f8e1
Priangshu2008 <78959073+Priangshu2008@users.noreply.github.com>
2021-05-24 17:17:50 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,47 @@
-var garden, gardenImg, cat1, cat2, cat3, cat4, cat1Img, cat2Img, cat3Img, cat4Img, mouse1, mouse2, mouse3, mouse4, mouse1Img, mouse2Img, mouse3Img, mouse4Img
+var canvas,bg;
+var together;
+var tom, tomImg1,tomImg2;
+var jerry, jerryImg1,jerryImg2;
+
 function preload() {
-catImage = loadImage("cat1.png");
-catImage = loadImage("cat2.png");
-catImage = loadImage("cat3.png");
-catImage = loadImage("cat4.png");
-gardenImage = loadImage("garden.png");
-mouseImage = loadImage("mouse1.png");
-mouseImage = loadImage("mouse2.png");
-mouseImage = loadImage("mouse3.png");
-mouseImage = loadImage("mouse4.png");
+bg = loadImage("images/garden.png");
+tomImg1= loadAnimation("images/cat1.png");
+tomImg2= loadAnimation("images/cat2.png" , "images/cat3.png");
+tomImg3= loadAnimation("images/cat4.png");
+jerryImg1= loadAnimation("images/mouse1.png", );
+jerryImg2= loadAnimation("images/mouse2.png" , "images/mouse3.png");
+jerryImg3= loadAnimation("images/mouse4.png");
+
 }
 
 function setup(){
-    createCanvas(1000,800);
-    
-    text(mouseX + ','+ mouseY,10, 45);
+canvas =  createCanvas(1000,800);
+
+tom = createSprite(870, 600);
+tom.addAnimation("tomSleeping", tomImg1);
+tom.scale = 0.2;
+
+jerry = createSprite(200, 600);
+jerry.addAnimation("jerryStanding", jerryImg1);
+jerry.scale = 0.15;
 
 }
 
 function draw() {
 
-    background(255);
-    //Write condition here to evalute if tom and jerry collide
+    background(bg);
+
+if(tom.x - jerry.x < (tom.width - jerry.width)/2)
+{
+    tom.velocityX=0;
+    tom.addAnimation("tomLastImage", tomImg3);
+    tom.x =300;
+    tom.scale=0.2;
+    tom.changeAnimation("tomLastImage");
+    jerry.addAnimation("jerryLastImage", jerryImg3);
+    jerry.scale=0.15;
+    jerry.changeAnimation("jerryLastImage"); 
+  }
 
     drawSprites();
 }
@@ -29,11 +49,16 @@ function draw() {
 
 function keyPressed(){
 
-    if(keyCode === LEFT_ARROW){
-        cat.velocityX = -5;
-        cat.addAnimation("catRunning", catImg2);
-        cat.changeAnimation("catRunning");
+if(keyCode === LEFT_ARROW){
+    tom.velocityX = -5;
+    tom.addAnimation("tomRunning", tomImg2);
+    tom.changeAnimation("tomRunning");
 
-    }
+    jerry.addAnimation("jerryTeasing", jerryImg2);
+    jerry.frameDelay = 25;
+    jerry.changeAnimation("jerryTeasing");
+   }
 }
-    
+
+
+
